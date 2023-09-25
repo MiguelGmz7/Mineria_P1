41ca51bf306a0fa2a0d2d5a379c7fcc7ab1c074c
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2023-09-24 19:12:25 -0600

    sacamos los datos de nuestra muestra, revisar x_train

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import OneR
 import pandas
 
 def main ():
-    print("hola mundo")
+    filename = "../datasets/golf-dataset-categorical.csv"  # Raiz del archivo
+    porcentaje_entranimento = .7 
+    clase = 'Play'
+
+    dataset = pandas.read_csv(filename)
+
+    muestra = dataset.sample(frac=porcentaje_entranimento)
+    y_train = muestra[clase]
+    x_train = muestra.drop(y_train)
