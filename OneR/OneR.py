380040b07ad14f6efe5655a2c26d624fa035603c
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2023-09-25 01:17:42 -0600

    sin comentarios

--- OneR/OneR.py
+++ OneR/OneR.py
@@ -2,16 +2,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import os
 
-# Cargar el archivo CSV
-
-#x_test = test_data(columns=[clase_column]) #los puros atributos
-
-
-# Calcular la frecuencia de cada valor en la columna de clase en el conjunto de entrenamiento
-
-# # Encontrar la clase más frecuente en el conjunto de entrenamiento
-# print(f"Clase más frecuente en el conjunto de entrenamiento: {most_frequent_class}")
-
 # Inicializar un diccionario para almacenar las reglas de One-R
 def frequencie_table(train_data, clase_column, test_data):
     one_r_rules = {}
@@ -35,21 +25,23 @@ def frequencie_table(train_data, clase_column, test_data):
 # Aplicar las reglas de One-R al conjunto de prueba y calcular la tasa de error
 
 def app_OneR(one_r_rules, test_data, most_frequent_class, clase_column):
-    misclassified_instances = 0
-    
+
+    correct_predictions = 0  # Inicializar el contador de predicciones correctas
 
     for _, instance in test_data.iterrows():
         predicted_class = None
-        min_error = float('inf')
-        
+
         for column, (best_rule, _) in one_r_rules.items():
             if (instance[column] == best_rule).any():
                 predicted_class = most_frequent_class
-        
-        actual_class = instance[clase_column]
-        
-        if predicted_class != actual_class:
-            misclassified_instances += 1
 
-    error_rate = misclassified_instances / len(test_data)
-    print(f"Tasa de error en el conjunto de prueba: {error_rate:.2f}")
+        actual_class = instance[clase_column]
+
+        if predicted_class == actual_class:  # Comprobar si la predicción es correcta
+            correct_predictions += 1
+
+    accuracy = correct_predictions / len(test_data)  # Calcular la tasa de acierto
+    print(f"Aciertos: {correct_predictions} ")
+    print("Total: ",len(test_data))
+    print(f"Tasa de acierto en el conjunto de prueba: {accuracy}")
+
--- OneR/main.py
+++ OneR/main.py
@@ -3,28 +3,14 @@ import pandas as pd
 
 
 def main ():
-    # filename = "../datasets/golf-dataset-categorical.csv"  # Dataset filename
-    # train_sample_size = .7  # Training percentage
-    # class_column_name = 'Play'
-
-    # dataset = pandas.read_csv(filename)
-
-    # x_train = dataset.sample(frac=train_sample_size) # agarramos el 80 porciento de toda nuestra tabla (atributos y clases)
-    # y_train = x_train[class_column_name] # copiamos en una variables las puras clases
-    # x_train = x_train.drop(columns=class_column_name) # eliminamos las clases de nuestro datos, (nos quedamos con los puros atributos)
-
-    # x_test = dataset.drop(x_train.index) # Los datos que nos faltan osea el 20%, en este caso 3 instancias
-    # y_test = x_test[class_column_name] #las puras clases
-    # x_test = x_test.drop(columns=[class_column_name]) #los puros atributos
-
     file_path = '../datasets/golf-dataset-categorical.csv'
     data = pd.read_csv(file_path)
     train_sample_size = .7
+
     # Nombre de la columna de la clase
     clase_column = 'Play'
 
-# Dividir el conjunto de datos en entrenamiento (70%) y prueba (30%)
-# train_data, test_data = train_test_split(data, test_size=0.3, random_state=42)
+
     train_data = data.sample(frac=train_sample_size) # agarramos el 80 porciento de toda nuestra tabla (atributos y clases)
     y_train = train_data[clase_column] # copiamos en una variables las puras clases
     x_train = train_data.drop(columns=clase_column) # eliminamos las clases de nuestro datos, (nos quedamos con los puros atributos)
@@ -33,9 +19,6 @@ def main ():
     y_test = test_data[clase_column] #las puras clases
 
     OneR.frequencie_table(train_data, clase_column, test_data)
-    
-
-
     
 
 if __name__ == '__main__':
