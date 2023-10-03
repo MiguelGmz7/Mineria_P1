6253269528a7e7fa6b471096a46d8b7589f0cdf6
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2023-10-02 23:39:29 -0600

    se crean tablas de frecuencia

--- Bayes/Bayes.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-
-class Bayes: 
-    def __init__(self, x):
-        
--- Bayes/main.py
+++ Bayes/main.py
@@ -1,9 +1,37 @@
 import pandas as pd
-import Bayes
+def split(data):
+    frequency = {}
+    index = 0
+
+    for column_name in data.columns:
+        frequency[column_name] = {}
+        for value in data[column_name]:
+            index += 1
+            if data[column_name].dtypes == 'object':
+                if value not in frequency[column_name]:
+                    frequency[column_name][value] = 1
+                else:
+                    frequency[column_name][value] += 1
+            else:
+                frequency[column_name][index] = value
+        index = 0
+        #-----
+        #if data[column_name].dtypes == 'float64':
+
+    return frequency
+
 def main():
-    file_path = "../datasets/golf-dataset-categorical.csv"
-    data = pd.read_csv(file_path)
-    train_sample_size = .7
-    
+    file_path = "../datasets/iris.csv"
+    df = pd.read_csv(file_path)
+    #train_sample_size = .7
+    #print(df)
+    #print(df.columns)
+    #print(df.dtypes)
+    #print(df.describe())
+    #print(df.head())
+    # como uso el metodo split de la clase Bayes
+    frecuency = split(df)
+    print(frecuency)
+
 if __name__ == '__main__':
     main()
